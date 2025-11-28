d8e951fc351103613278cb8633cc8ef6794efbe0
Sailorns <vojislav.simic@gmail.com>
2025-11-28 08:04:03 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,45 +1,39 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+// database.js - Memory Database (Ne gubi se na redeploy)
+let messages = [];
+let nextId = 1;
 
-const dbPath = path.join(__dirname, 'birthday.db');
-const db = new sqlite3.Database(dbPath);
-
-// Inicijaliziraj bazu podataka
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS messages (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        message TEXT NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )`);
-});
-
-// Funkcije za rad s bazom podataka
 function getAllMessages() {
-    return new Promise((resolve, reject) => {
-        db.all("SELECT * FROM messages ORDER BY created_at DESC", (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
+    // Vraćamo kopiju niza da se ne bi mogao direktno modifikovati
+    return Promise.resolve([...messages].reverse()); // Najnovije poruke prvo
 }
 
 function addMessage(name, message) {
-    return new Promise((resolve, reject) => {
-        db.run("INSERT INTO messages (name, message) VALUES (?, ?)", [name, message], function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ id: this.lastID });
-            }
-        });
-    });
+    const newMessage = {
+        id: nextId++,
+        name: name.replace(/</g, '&lt;').replace(/>/g, '&gt;'), // XSS zaštita
+        message: message.replace(/</g, '&lt;').replace(/>/g, '&gt;'), // XSS zaštita
+        created_at: new Date().toISOString()
+    };
+    
+    messages.push(newMessage);
+    console.log(`Poruka sačuvana: ID=${newMessage.id}, Od=${name}`);
+    
+    return Promise.resolve({ id: newMessage.id });
+}
+
+function getMessageCount() {
+    return messages.length;
+}
+
+function clearAllMessages() {
+    messages = [];
+    nextId = 1;
+    return Promise.resolve();
 }
 
 module.exports = {
     getAllMessages,
-    addMessage
+    addMessage,
+    getMessageCount,
+    clearAllMessages
 };
